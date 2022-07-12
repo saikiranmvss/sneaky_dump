601c1ef6f55defa75e21b11878b0fa9317124294
Sai Kiran <89825962+saikiranmvss@users.noreply.github.com>
2022-07-12 07:24:58 +0530

    Add files via upload

--- profile_settings.php
+++ profile_settings.php
@@ -22,8 +22,8 @@
    <div class="main-back">
    <div class="prof-container">
       <div class="img-border" style="border: 5px solid rgb(255 255 255);border-radius: 60px;position: relative;">
-   <img src="test.jpg" width="100" height="100" style="border-radius: 60px;">
-   <i style="position: absolute;color: rgb(255 255 255);bottom: -8px;right: -6px;background: rgb(0 0 0);padding: 10px;border-radius: 50%;" class='far fa-edit'></i>
+   <img src="man.jpg" width="100" height="100" style="border-radius: 60px;">
+   <i style="position: absolute;color: rgb(255 255 255);bottom: -8px;right: -6px;background: rgb(0 0 0);padding: 10px;border-radius: 50%;" class='far fa-edit' id="myBtn"></i>
 </div>
 <h4 class="name">john Doe</h4>
    </div>
@@ -34,7 +34,25 @@
 <div class="tiles"><i class="fa fa-phone"></i><input tyoe="tel" id="phone" name="phone" placeholder="Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Phone Number'"></div>
 <div class="tiles"><i class='far fa-address-card'></i><textarea id="address" class="address" row='10' placeholder="Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Address'"></textarea></div>
 </div>
-<div class="button-submit"><button type="button" style="background-color: rgb(4 34 49);" class="btn btn-primary">Primary</button></div>
+<div class="button-submit"><button type="button" style="background-color: rgb(4 34 49);" class="btn btn-primary">Save</button></div>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <!-- Modal content -->
+<div class="modal-content">
+    <span class="close" style="cursor: pointer;position: absolute;right: 10px;top: 0;font-size: 40px;">&times;</span>
+    <div style='display: flex;justify-content:space-around;'>
+    <div style="display: grid;width: 25%;">
+<i class="fas fa-user-circle" style="width: 100%;height: 100%;font-size: 125px;color: white;"></i>
+<p style="color: white;text-align: center;font-size: 20px;word-wrap: break-word;" >Set default Image</p>
+    </div>
+    <div style="display: grid;width: 25%;">
+<i class="fa fa-image" style="width: 100%;height: 100%;font-size: 125px;color: white;"></i>
+<p style="color: white;text-align: center;font-size: 20px;word-wrap: break-word;">Upload Image</p>
+    </div>
+    </div>
+
+  </div>
+</div>
 <style>
 
 
@@ -102,5 +120,74 @@ input:focus-visible{
    .prof-container{width: 100%;height: 100%;display: inline-grid;padding: 50px;align-items: center;justify-content: center;};
    
 
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+   background: url('https://wallpaperaccess.com/full/731608.jpg');
+    margin: auto;
+    padding: 15px;
+    position: absolute;
+    border: 1px solid #888;
+    width: 100%;
+    height: 200px;
+    bottom: 0;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
    </style>
+   <script>
+      // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+$('#myBtn').click(function(){
+   modal.style.display = "block";
+})  
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+   </script>
   </body>
